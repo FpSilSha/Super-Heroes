3fb9ca5df843e8e439ebe7fa5e8053de4052af40
Fp38SilSha <faperez38s@gmail.com>
2019-03-04 10:01:23 -0600

    Added HeroesNHeroines class to represent Super Hero/Heroine table. Added several properties for this class: SuperName Alter_Ego_Name Primary_SuperAbility Secondary_SuperAbility CatchPhrase

---
+++ SuperHeroes/Models/HeroesNHeroines.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class HeroesNHeroines
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string SuperName { get; set; }
+        [Required]
+        public string Alter_Ego_Name { get; set; }
+        [Required]
+        public string Primary_SuperAbility { get; set; }
+        public string Secondary_SuperAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}

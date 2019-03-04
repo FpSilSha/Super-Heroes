c68a8fb325c79642ae3215dec9e0c74367603401
Fp38SilSha <faperez38s@gmail.com>
2019-03-04 10:41:05 -0600

    Added four views for the controller: Create/Read(details)/Update(edit)/Delete

---
+++ SuperHeroes/Controllers/HeroesNHeroinesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class HeroesNHeroinesController : Controller
+    {
+        // GET: HeroesNHeroines
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HeroesNHeroines/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HeroesNHeroines/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HeroesNHeroines/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HeroesNHeroines/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HeroesNHeroines/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HeroesNHeroines/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HeroesNHeroines/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
